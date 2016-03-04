90d4278122b397dd23ab5c1eea3ee95eac21fd0d
Alex-Pedersen <1038281@ucn.dk>
2016-03-04 09:28:56 +0100

    Oprettelse af klasser

---
+++ RepoDAOCrud/RepoDAOCrud/Interfaces/IShapeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoDAOCrud.Interfaces
+{
+    interface IShapeRepository
+    {
+    }
+}

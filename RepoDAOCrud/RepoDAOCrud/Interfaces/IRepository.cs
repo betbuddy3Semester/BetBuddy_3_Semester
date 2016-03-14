21b2639f10daf929313766838d8d5a285be4867e
Alex-Pedersen <1038281@ucn.dk>
2016-03-14 09:42:37 +0100

    Oprettelse af projekt

--- RepoDAOCrud/RepoDAOCrud/Interfaces/IRepository.cs
+++ RepoDAOCrud/RepoDAOCrud/Interfaces/IRepository.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace RepoDAOCrud.Interfaces
 {
     public interface IRepository<T>
     {
+        void Insert(T entity);
+        void Delete(T entity);
+        IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
+        IQueryable<T> GetAll();
+        T GetById(int id);
     }
 }
